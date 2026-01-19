989f9f8969e7c8589d7b8fdbea9aeb113829dc4e
Алена <alyona@example.com>
2026-01-20 02:13:30 +0300

    commit 2

---
+++ src/js/__tests__/sortHeroes.test.js
@@ -0,0 +1,24 @@
+import sortHeroes from '../src/sortHeroes';
+
+describe('sortHeroes', () => {
+  test('sorts heroes by health descending', () => {
+    const heroes = [
+      { name: 'мечник', health: 10 },
+      { name: 'маг', health: 100 },
+      { name: 'лучник', health: 80 },
+    ];
+
+    const expected = [
+      { name: 'маг', health: 100 },
+      { name: 'лучник', health: 80 },
+      { name: 'мечник', health: 10 },
+    ];
+
+    expect(sortHeroes(heroes)).toEqual(expected);
+  });
+
+  test('toBe does not work for objects', () => {
+    const heroes = [{ name: 'маг', health: 100 }];
+    expect(sortHeroes(heroes)).not.toBe(heroes);
+  });
+});
